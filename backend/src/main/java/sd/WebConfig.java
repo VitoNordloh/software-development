4d82c9ed16d1bc4ffcd7a859c40851f76f8cb5cc
Vito Alexander Nordloh <vito.nordloh@vitonordloh.de>
2019-03-27 16:53:30 +0000

    Remove connection uri constant

--- backend/src/main/java/sd/WebConfig.java
+++ backend/src/main/java/sd/WebConfig.java
@@ -2,6 +2,7 @@ package sd;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,12 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${database.uri}")
+    private String databaseUri;
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
 
     public @Bean MongoClient mongoClient() {
-        MongoClientURI uri = new MongoClientURI("mongodb://admin:guc3XK9drxmH3vhZ@sd-shard-00-00-gmjiq.mongodb.net:27017,sd-shard-00-01-gmjiq.mongodb.net:27017,sd-shard-00-02-gmjiq.mongodb.net:27017/test?ssl=true&replicaSet=SD-shard-0&authSource=admin&retryWrites=true");
+        MongoClientURI uri = new MongoClientURI(databaseUri);
         return new MongoClient(uri);
     }
 }
